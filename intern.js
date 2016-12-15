5d6cc5d957ae790def529144d5e40c5d41724639
Paul Shannon <pshannon@sitepen.com>
2016-12-15 09:59:42 -0700

    Added config for Appium 1.6.3 and various environments

--- intern.js
+++ intern.js
@@ -5,7 +5,12 @@ define({
 	},
 
 	environments: [
-		{ browserName: 'iphone', version: '9.3', deviceName: 'iPhone 6' }
+		// From the configurator https://wiki.saucelabs.com/display/DOCS/Platform+Configurator#/
+		{ browserName: 'Safari', appiumVersion: '1.6.3', deviceName: 'iPhone 6 Simulator',
+			deviceOrientation: 'portrait', platformVersion: '9.3', platformName: 'iOS' }
+		// Typical Intern config
+		// { browserName: 'iphone', version: '9.3', deviceName: 'iPhone 6', appiumVersion: '1.6.3' }
+		// Chrome config to show it works on Selenium
 		// { browserName: 'chrome' }
 	],
 

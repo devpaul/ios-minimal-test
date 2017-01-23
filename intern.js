ea0dd595b84154b8d4c085b364cee0d3c0a898c2
Paul Shannon <pshannon@sitepen.com>
2017-01-23 15:34:10 -0700

    Updated it to ios10 & added log capture

--- intern.js
+++ intern.js
@@ -1,28 +1,37 @@
-define({
-	capabilities: {
-		project: 'Intern Repeated Test',
-		name: 'iOS-intern-test'
-	},
+define([], function () {
+	// From the configurator https://wiki.saucelabs.com/display/DOCS/Platform+Configurator#/
+	var caps = {};
+	caps['browserName'] = 'Safari';
+	caps['appiumVersion'] = '1.6.3';
+	caps['deviceName'] = 'iPhone 6 Simulator';
+	caps['deviceOrientation'] = 'portrait';
+	caps['platformVersion'] = '10.0';
+	caps['platformName'] = 'iOS';
 
-	environments: [
-		// From the configurator https://wiki.saucelabs.com/display/DOCS/Platform+Configurator#/
-		{ browserName: 'Safari', appiumVersion: '1.6.3', deviceName: 'iPhone 6 Simulator',
-			deviceOrientation: 'portrait', platformVersion: '9.3', platformName: 'iOS' }
-		// Typical Intern config
-		// { browserName: 'iphone', version: '9.3', deviceName: 'iPhone 6', appiumVersion: '1.6.3' }
-		// Chrome config to show it works on Selenium
-		// { browserName: 'chrome' }
-	],
+	return {
+		capabilities: {
+			project: 'Intern Repeated Test',
+			name: 'iOS-intern-test'
+		},
 
-	tunnel: 'SauceLabsTunnel',
+		environments: [
+			caps
+			// Typical Intern config
+			// { browserName: 'iphone', version: '9.3', deviceName: 'iPhone 6', appiumVersion: '1.6.3' }
+			// Chrome config to show it works on Selenium
+			// { browserName: 'chrome' }
+		],
 
-	loaderOptions: {
-		packages: [ { name: 'tests', location: '.' } ]
-	},
+		tunnel: 'SauceLabsTunnel',
 
-	suites: [ 'tests/stress' ],
+		loaderOptions: {
+			packages: [ { name: 'tests', location: '.' } ]
+		},
 
-	functionalSuites: [ ],
+		suites: [ 'tests/stress' ],
 
-	excludeInstrumentation: true
+		functionalSuites: [ ],
+
+		excludeInstrumentation: true
+	};
 });
